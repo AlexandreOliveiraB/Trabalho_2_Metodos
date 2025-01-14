df4689787989b94f6b3b9868d6bb86c0d28ba7b6
AlexandreOliveiraB <xandeoliveira114@gmail.com>
2025-01-14 10:56:01 -0300

    Segundo teste para abrir um arquivo que esta no mesmo diretorio passsando

--- conta_palavras.cpp
+++ conta_palavras.cpp
@@ -5,17 +5,16 @@
  * \file  conta_palavras.cpp
  */
 #include "conta_palavras.hpp"
-#include "catch.hpp"
 
+#include <string>
+#include <iostream>
 #include <fstream>
 #include <stdexcept>
-#include <iostream>
-#include <string>
 #include <vector>
 #include <sstream>
 #include <map>
 #include <algorithm>
-
+#include "catch.hpp"
 void abrir_arquivo(const std::string& nome_arquivo) {
     std::ifstream arquivo(nome_arquivo);
     if (!arquivo.is_open()) {
--- testa_conta_palavras.cpp
+++ testa_conta_palavras.cpp
@@ -4,22 +4,20 @@
 /**
  * \file testa_conta_palavras.cpp
  */
-
-#include "conta_palavras.hpp"
-
 #define CATCH_CONFIG_MAIN
 #define CATCH_CONFIG_NO_POSIX_SIGNALS
+
+#include "conta_palavras.hpp"
+#include <string>
+#include <fstream>
+#include <map>
 #include "catch.hpp"
 
-#include <fstream>
-#include <string>
-#include <map>
-
-// Testa a abertura de arquivo inexistente 
+// Testa a abertura de arquivo inexistente
 TEST_CASE("Arquivo inexistente deve falhar") {
-    REQUIRE_THROWS_AS(abrir_arquivo("inexistente.txt"), const std::ios_base::failure&); //Aqui eh para falhar, ele testa se nao abre nada
+    REQUIRE_THROWS_AS(abrir_arquivo("nao.txt"), const std::ios_base::failure&);
 }
-// Testa a abertura de arquivo existente 
+// Testa a abertura de arquivo existente
 TEST_CASE("Arquivo existente deve abrir com sucesso") {
-    REQUIRE_NOTHROW(abrir_arquivo("exemplo.txt"));  
+    REQUIRE_NOTHROW(abrir_arquivo("exemplo.txt"));
 }
