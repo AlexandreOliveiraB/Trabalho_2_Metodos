922fd085f0c9437bc2463836d330e89f57728e93
AlexandreOliveiraB <xandeoliveira114@gmail.com>
2025-01-14 12:16:14 -0300

    Decimo quarto teste para contar palavras em um arquivo vazio passando

--- conta_palavras.cpp
+++ conta_palavras.cpp
@@ -47,13 +47,5 @@ std::map<std::string, int> contar_palavras(const std::string& texto) {
     while (stream >> palavra) {
         contagem[palavra]++;
     }
-    return {
-        {"Esta", 2},
-        {"é", 2},
-        {"uma", 2},
-        {"frase", 2},
-        {"de", 2},
-        {"teste.", 1},
-        {"Teste.", 1}
-    };;
+    return contagem;
 }
