3c24605bdec7a2662f9c286440139c809fa97426
AlexandreOliveiraB <xandeoliveira114@gmail.com>
2025-01-14 09:57:03 -0300

    Segundo teste para abrir um arquivo que esta no mesmo diretorio falhando

--- testa_conta_palavras.cpp
+++ testa_conta_palavras.cpp
@@ -15,7 +15,11 @@
 #include <string>
 #include <map>
 
-// Testa a abertura de arquivo inexistente 1
+// Testa a abertura de arquivo inexistente 
 TEST_CASE("Arquivo inexistente deve falhar") {
-    REQUIRE_THROWS_AS(abrir_arquivo("inexistente.txt"), const std::ios_base::failure&); //Aqui esta falhandom, pois nao deveria ter um arquivo, ele testa se nao abre nada
+    REQUIRE_THROWS_AS(abrir_arquivo("inexistente.txt"), const std::ios_base::failure&); //Aqui eh para falhar, ele testa se nao abre nada
+}
+// Testa a abertura de arquivo existente 
+TEST_CASE("Arquivo existente deve abrir com sucesso") {
+    REQUIRE_NOTHROW(abrir_arquivo("exemplo.txt"));  
 }
